cf3b0c994fc9a00517450975e47a87ce10cc806c
arisafaribinridwan <arisafari.ridwan@gmail.com>
2023-11-09 17:01:10 +0700

    initil setup travel-blog landing page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        "inter":['inter']
+      }
+    },
+    colors : {
+      "ungu" : '#5D50C6',
+      "pink" : '#F85E9F',
+      "orange" : '#FF5722', 
+      "yellow" : '#FACD49'
+        }
+  },
+  plugins: [],
+}
+
