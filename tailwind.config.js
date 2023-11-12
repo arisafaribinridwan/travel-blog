bb49e6376729cf2d574f339529b3ee17b03e5700
arisafari.ridwan <arisafari.ridwan@gmail.com>
2023-11-12 08:28:15 +0700

    navbar mobile slicing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "index.html",
     "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {
-      fontFamily : {
-        "inter":['inter']
+      fontFamily: {
+        "inter": ['inter']
+      },
+
+      colors: {
+        "ungu": '#5D50C6',
+        "pink": '#F85E9F',
+        "orange": '#FF5722',
+        "yellow": '#FACD49'
       }
+
     },
-    colors : {
-      "ungu" : '#5D50C6',
-      "pink" : '#F85E9F',
-      "orange" : '#FF5722', 
-      "yellow" : '#FACD49'
-        }
   },
   plugins: [],
 }
